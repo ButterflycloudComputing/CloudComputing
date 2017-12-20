ab1cd5c775c05bcf0b7aedbce564ad1268a65fa9
tjuHaoXiaotian <15122938076@163.com>
2017-12-20 22:23:39 +0800

    整合前端搜索分词

--- Weibo/src/main/java/cn/edu/tju/scs/hxt/web/MainController.java
+++ Weibo/src/main/java/cn/edu/tju/scs/hxt/web/MainController.java
@@ -7,7 +7,13 @@ import cn.edu.tju.scs.hxt.pojo.dto.PositionInfo;
 import cn.edu.tju.scs.hxt.pojo.vo.PageResultVO;
 import cn.edu.tju.scs.hxt.pojo.vo.ResponseCodeVO;
 import cn.edu.tju.scs.hxt.pojo.vo.WeiBo;
+import com.chenlb.mmseg4j.analysis.ComplexAnalyzer;
 import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -34,43 +41,58 @@ public class MainController {
 
     @RequestMapping(value = "/search", method = RequestMethod.GET,produces = {"application/json;charset=UTF-8"})
     public @ResponseBody
-    ResponseCodeVO<PageResultVO<WeiBo>> search( @RequestParam(value = "keyword", required = false) String keyWord, @RequestParam(value = "pageNum",required = false)Integer pageNum){
+    ResponseCodeVO<PageResultVO<WeiBo>> search( @RequestParam(value = "keyword", required = false) String keyWord, @RequestParam(value = "pageNum",required = false)Integer pageNum) throws IOException {
         pageNum = pageNum == null ? 0: pageNum;
         int pageSize = 10;
-        int start,end;
+        int startPos,endPos;
         List<WeiBo> weiBos;
+        String key;
         if(!StringUtils.isEmpty(keyWord)) {
-            String values = wordsTable.getValue(keyWord);
-            String[] valuesArray = values.split(" ");
+            // 包含所有有当分词语的微博
+            List<PositionInfo> results = new ArrayList<>();
+            String values;
+            String[] valuesArray;
             PositionInfo info;
             String[] infoArray;
-            String pos, startPos, stopPos;
-            // 所有有当前词语的微博
-            List<PositionInfo> results = new ArrayList<>();
-            if (valuesArray.length > 0) {
-                for (String item : valuesArray) {
-                    infoArray = item.split("-->");
-                    assert (infoArray.length == 2);
-                    info = new PositionInfo();
-                    info.setScore(Integer.valueOf(infoArray[1]));
-                    pos = infoArray[0].substring(1, infoArray[0].length() - 1);
-                    infoArray = pos.split(":");
-                    info.setStartPos(Long.valueOf(infoArray[0]));
-                    info.setLength(Integer.valueOf(infoArray[1]));
-                    results.add(info);
+            String pos;
+
+            // 分词
+            Analyzer ay = new ComplexAnalyzer();
+            TokenStream ts = ay.tokenStream("field", keyWord);
+            CharTermAttribute ch = ts.addAttribute(CharTermAttribute.class);
+            ts.reset();
+            while (ts.incrementToken()) {
+//                System.out.println(ch.toString());
+                key = ch.toString();
+
+                values = wordsTable.getValue(key);
+                valuesArray = values.split(" ");
+
+                if (valuesArray.length > 0) {
+                    for (String item : valuesArray) {
+                        infoArray = item.split("-->");
+                        assert (infoArray.length == 2);
+                        info = new PositionInfo();
+                        info.setScore(Integer.valueOf(infoArray[1]));
+                        pos = infoArray[0].substring(1, infoArray[0].length() - 1);
+                        infoArray = pos.split(":");
+                        info.setStartPos(Long.valueOf(infoArray[0]));
+                        info.setLength(Integer.valueOf(infoArray[1]));
+                        results.add(info);
+                    }
                 }
-                Collections.sort(results);
-
-                // 获取当前页起止位置
-                start = pageSize * (pageNum - 1);
-                end = pageSize * pageNum > results.size() ? results.size(): pageSize * pageNum;
-
-                weiBos = wordsTable.randomRead(config.getInputFile(), results.subList(start,end));
-            } else {
-                weiBos = new ArrayList<>();
             }
 
-
+            if(results.size() > 0) {
+                Collections.sort(results);
+                // 获取当前页起止位置
+                startPos = pageSize * (pageNum - 1);
+                endPos = pageSize * pageNum > results.size() ? results.size() : pageSize * pageNum;
+                weiBos = wordsTable.randomRead(config.getInputFile(), results.subList(startPos, endPos));
+            }
+            else {
+                weiBos = new ArrayList<>();
+            }
 
             PageResultVO<WeiBo> result = new PageResultVO<WeiBo>(pageSize, results.size());
             result.setCurrentPage(pageNum);
